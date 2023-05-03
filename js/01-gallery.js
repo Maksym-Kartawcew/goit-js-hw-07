c468a74a7dbd4c89ba75e27b4f4a5baeb5ea2e16
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-05-03 12:51:20 +0200

    Homework7 - last

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-const galleryUl = document.querySelector('.gallery');
+const galleryUl = document.querySelector(".gallery");
 const galleryItem = galleryItems
-.map( (item) => 
-    `<li class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-      <img
-        class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
-      />
-    </a>
-  </li>`
-).join("");
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>`
+  )
+  .join("");
 galleryUl.innerHTML = galleryItem;
 
 galleryUl.addEventListener("click", onGalleryItemClick);
@@ -23,32 +25,30 @@ function onGalleryItemClick(event) {
 
   const instance = basicLightbox.create(
     `
-    <div class="modal">
+      <div class="modal">
         <img src="${event.target.dataset.source}" width="800" height="600">
-
-    </div>`,
-
+      </div>`,
     {
-    onShow: (instance) => { window.addEventListener('keyDown', onModalKeyDown);}
-		onClose: (instance) => { window.removeEventListener('keyDown', onModalKeyDown); }
+      onShow: () => {
+        document.addEventListener("keydown", onModalKeyDown);
+      },
+
+      onClose: () => {
+        document.removeEventListener("keydown", onModalKeyDown);
+      },
     }
   );
 
-
   instance.show();
 
-  const modalElement = document.querySelector('.modal')
-  modalElement.addEventListener('click', () => { instance.close();})
-}
+  const modalElement = document.querySelector(".modal");
+  modalElement.addEventListener("click", () => {
+    instance.close();
+  });
 
-function onModalKeyDown(event) {
-  if (event.code === 'Escape') {
+  function onModalKeyDown(event) {
+    if (event.code === "Escape") {
       instance.close();
-      }
     }
-
-
-
-
-
-
+  }
+}
